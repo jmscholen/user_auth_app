62782e6e153f419e6ffd92d45506fde92234e0e4
jmscholen <jmscholen@gmail.com>
2014-05-14 09:06:57 -0400

    updated to reflect workign log out link

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  after_create :congrats_email
-
-  def congrats_email
-    mail(to: self.email, subject: "Welcome Message")
-  end
-
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  
+	protected
+	def confirmation_required?
+	  false
+	end
 end
