61c55def4d0f5570c923dc5072e7c478c4cffc14
jmscholen <jmscholen@gmail.com>
2014-05-14 06:55:35 -0400

    added mail action and update welcome to log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  after_create :congrats_email
+
+  def congrats_email
+    mail(to: self.email, subject: "Welcome Message")
+  end
+
 end
